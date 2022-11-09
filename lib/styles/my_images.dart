db4d578e975670a50ba17dd1c4b4acfb26d34885
Timur Kreminskiy <timurkreminskij@gmail.com>
2022-11-09 23:43:44 +0600

    Custom fonts, Description screen UI

---
+++ lib/styles/my_images.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const desc_image = 'assets/images/desc_image.svg';
+const next_single_arrow = 'assets/images/next_single_arrow.svg';
+const welcome_image = 'assets/images/welcome_screen_image.svg';
+const bottom_background = 'assets/images/bottom_background.svg';
